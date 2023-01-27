9cb10af7f516ba4f8959b0ac2ff33e16329e20d8
VinayAvala <vinayav@infinite.com>
2023-01-27 14:56:10 +0530

    Add project files.

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,60 @@
+﻿using EHS_MVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EHS_MVC.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AccountsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+
+            return View(login);
+        }
+
+        [HttpPost]
+        public IActionResult LogOut()
+        {
+            HttpContext.Session.Remove("token");
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Accounts/signup")]
+        public IActionResult SignUp(SignUpViewModel signUp)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Login","Accounts");
+            }
+            else
+            {
+                return View(signUp);
+            }
+        }
+    }
+}
